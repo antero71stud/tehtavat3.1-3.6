30df48d1c92050f7b4bb3785736c8d40dd976c98
Antero Oikkonen <antero.oikkonen@gmail.com>
2018-02-11 22:53:21 +0200

    3.9 cors added

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const cors = require('cors')
 
 app.use(bodyParser.json())
+app.use(cors())
 app.use(morgan(':method :url :data :status :res[content-length] - :response-time ms'))
 /*app.use(morgan('tiny'))*/
 
