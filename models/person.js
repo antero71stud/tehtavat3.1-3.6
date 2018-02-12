16f64dae444b61170cc975a82411a0a00337f222
Antero Oikkonen <antero.oikkonen@gmail.com>
2018-02-12 23:36:25 +0200

    ympäristömuuttuja

--- models/person.js
+++ models/person.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
-const url = 'mongodb://<username>:<password>:@ds135186.mlab.com:35186/full-stack-persons'
+//const url = 'mongodb://<username>:<password>:@ds135186.mlab.com:35186/full-stack-persons'
+const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
 
