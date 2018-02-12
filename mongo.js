04b95c2ada1fbbfe3884df52d61ad8c6c0adbaa6
Antero Oikkonen <antero.oikkonen@gmail.com>
2018-02-12 21:34:18 +0200

    mongo.js ilman salasanaa ja käyttäjätunnusta

---
+++ mongo.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+// korvaa url oman tietokantasi urlilla. ethän laita salasanaa Gothubiin!
+// Kannattaa jättää erikoismerkit käyttäjätunnuksessa ja salassanassa
+// käyttämättä ja käyttää pitkää salalausetta. Yli 19 merkkiä riittänee
+// pitkälle.
+const url = 'mongodb://<username>:<password>:@ds135186.mlab.com:35186/full-stack-persons'
+
+mongoose.connect(url)
+
+var args = process.argv.slice(2);
+
+const Person = mongoose.model('Person', {
+  name: String,
+  number: String
+})
+
+const person = new Person({
+  name: args[0],
+  number: args[1]
+})
+
+person
+  .save()
+  .then(response => {
+    console.log(`lisätään henkilö ${args[0]} numero ${args[1]} luetteloon`)
+    mongoose.connection.close()
+  })
